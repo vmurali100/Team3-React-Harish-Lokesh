aee111b0cb9606cb24bbf2fe80d4b9244456aac2
leelavathikm1 <leelavathikm1@gmail.com>
2021-08-21 18:14:01 +0530

    delete

--- Javascript/CRUD/create.js
+++ Javascript/CRUD/create.js
@@ -1,9 +1,15 @@
-var person={
-    email:"",
-    password:""
+
+var users=JSON.parse(localStorage.getItem("users"));
+if(users){
+    displayData(users)
+}else{
+    users=[];
 }
-var users=[];
 function getValues(){
+    var person={
+        email:"",
+        password:""
+    }
    
 //    person.email=document.getElementById("email").value
 //    person.password=document.getElementById("password").value
@@ -14,15 +20,41 @@ function getValues(){
         person[a]=document.getElementById(a).value;
        
     }
-    users.push(person[a])
+    users.push(person)
+    localStorage.setItem("users",JSON.stringify(users))
     // console.log(person)
     displayData(users)
   
 }
 
 function displayData(users){
-    users.map(function(obj){
-        console.log(users)
+    document.getElementById("myTbody").innerHTML="";
+//console.log(users)
+    users.map(function(obj,i){
+       var myTr=document.createElement("tr")
+       for(a in obj){
+           var myTd=document.createElement("td")
+            myTd.innerHTML=obj[a]
+           myTr.appendChild(myTd)
+       }
+        
+       var editTd=document.createElement("td")
+       var editBtn=document.createElement("button")
+       editBtn.innerHTML="Edit"
+       editTd.appendChild(editBtn)
+       editBtn.setAttribute("class","btn btn-warning")
+       myTr.appendChild(editTd)
+      
+       var delTd=document.createElement("td")
+       var delBtn=document.createElement("button")
+       delBtn.innerHTML="delete"
+       delBtn.setAttribute("class"," btn btn-danger")
+       delBtn.setAttribute("onclick","deleteUser("+ i +")")
+       delTd.appendChild(delBtn)
+       myTr.appendChild(delTd)
+
+       
+       document.getElementById("myTbody").appendChild(myTr)
     })
 
 }
---
+++ Javascript/CRUD/delete.js
@@ -0,0 +1,5 @@
+function deleteUser(i){
+    users.splice(i , 1)
+    displayData(users)
+    localStorage.setItem("users", JSON.stringify(users))
+}
