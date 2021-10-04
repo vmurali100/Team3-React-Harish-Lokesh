8c49db5bfa7a4da1f009503d955569e23ade7edc
leelavathikm1 <leelavathikm1@gmail.com>
2021-10-04 16:20:31 +0530

    songs

---
+++ MediaPlayer/audioPlayer.js
@@ -0,0 +1,39 @@
+var jukeBox=document.querySelector(".player")
+jukeBox.addEventListener("click",function (event){
+	var songName=event.target.getAttribute("data-src")
+	var audioPlayer=document.querySelector("#player");
+	if(audioPlayer){
+		if(songName === audioPlayer.getAttribute("src")){
+			if(audioPlayer.paused){
+				audioPlayer.play();
+				event.target.id="playing"
+			}else{
+				audioPlayer.pause()
+				event.target.id="paused"
+			}
+		}
+		else{
+			audioPlayer.src=songName;
+			audioPlayer.play()
+			if (document.querySelector('#playing')) {
+                document.querySelector('#playing').id = '';
+              }
+            else {
+                document.querySelector('#paused').id = '';
+                
+            }
+            event.target.id = 'playing';
+
+		}
+	}
+	else{
+		var audioPlayer=document.createElement("audio");
+		audioPlayer.id="player";
+		document.body.appendChild(audioPlayer);
+		audioPlayer.src=songName;
+		audioPlayer.play()
+		event.target.id = 'playing';
+	}
+})
+
+ 
